f9619a5d86e99ad89e6929cf1980010f389ab45f
qq0 <59629875+qqzero0@users.noreply.github.com>
2025-05-08 11:58:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const os = require("os");
 const readlineSync = require("readline-sync");
 const Discord = require("discord.js-selfbot-v13");
 const client = new Discord.Client({ checkUpdate: false });
